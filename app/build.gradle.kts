a7a6847c5c92f6d90214cd32fd6871c835c13a1e
guilherme <progr.guilherme@gmail.com>
2025-01-12 00:09:41 -0300

    up versionCode and VersionName v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.wwwjsw.musicserver"
         minSdk = 30
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
