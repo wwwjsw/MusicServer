e897ae5c807149353112bb7b9adbe92a82391cab
guilherme machado <progr.guilherme@gmail.com>
2025-09-07 20:20:18 -0300

    update to sdk 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.wwwjsw.musicserver"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.wwwjsw.musicserver"
--- app/src/main/java/com/wwwjsw/musicserver/MainActivityContent.kt
+++ app/src/main/java/com/wwwjsw/musicserver/MainActivityContent.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Icon
@@ -95,7 +94,6 @@ fun MainActivityContent(
                                         )
                                         selectionFilter = item.filter
                                     },
-                                    indication = rememberRipple(bounded = true),
                                     interactionSource = remember {
                                         MutableInteractionSource()
                                     }
