3fcf74719135df7c9a1ee77b890d552a2744af13
guilherme machado <progr.guilherme@gmail.com>
2026-02-25 23:26:23 -0300

    build: Bump versionCode to 6
    - Increment `versionCode` from 5 to 6 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.wwwjsw.musicserver"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
