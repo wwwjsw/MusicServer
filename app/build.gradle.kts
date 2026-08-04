181dbaf2620c610d91bc311a80985da4c767d631
guilherme machado <progr.guilherme@gmail.com>
2026-08-04 01:23:13 -0300

    Update versionCode to 7 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.wwwjsw.musicserver"
         minSdk = 30
         targetSdk = 36
-        versionCode = 6
+        versionCode = 7
         versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
