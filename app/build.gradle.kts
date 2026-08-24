f77c727e7626acdda513a1fe2bf69846484880d5
guilherme machado <progr.guilherme@gmail.com>
2026-08-23 23:49:41 -0300

    build: upgrade AGP, Kotlin, Ktor, and Gradle wrapper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
 }
 
@@ -34,14 +33,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.get()
     }
     packaging {
         resources {
@@ -60,7 +53,6 @@ android {
 dependencies {
     implementation(libs.gson)
 
-    // TODO: update to  3.x.x
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.content.negotiation)
